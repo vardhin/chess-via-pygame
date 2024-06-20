a78d9143d52f8e715725b708aff6f39d13616a5d
Vardhin <115967495+vardhin@users.noreply.github.com>
2024-06-20 17:31:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+HEIGHT = 600
+WIDTH = 800
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+
