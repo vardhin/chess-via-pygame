10542354f2382413b4fd3196577cb47c030686ee
shashank <166425677+shashankshekharmishra@users.noreply.github.com>
2024-06-20 17:56:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ HEIGHT = 600
 WIDTH = 800
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
+pygame.display.set_caption('Initial Window Title')
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     pygame.display.update()
-    '''hi 
-    bro'''
+    
+  
 
